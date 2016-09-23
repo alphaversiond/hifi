5e7dc9c356a535ef11561a10cb01b502b5432e34
Gabriel Calero <gcalero1984@gmail.com>
2016-09-23 20:13:47 -0300

    Port gpu-gl and gl to android

--- libraries/gpu-gl-android/src/gpu/gl/GLBackend.cpp
+++ libraries/gpu-gl-android/src/gpu/gl/GLBackend.cpp
@@ -10,3 +10,42 @@
 //
 #include "GLBackend.h"
 
+#include <gl/QOpenGLContextWrapper.h>
+
+using namespace gpu;
+using namespace gpu::gl;
+
+static GLBackend* INSTANCE{ nullptr };
+static const char* GL_BACKEND_PROPERTY_NAME = "com.highfidelity.gl.backend";
+
+//static BackendPointer GLBackend::createBackend() { return std::make_shared<GLBackend>(); }
+BackendPointer GLBackend::createBackend() {
+    // FIXME provide a mechanism to override the backend for testing
+    // Where the gpuContext is initialized and where the TRUE Backend is created and assigned
+    auto version = QOpenGLContextWrapper::currentContextVersion();
+    std::shared_ptr<GLBackend> result;
+	qDebug() << "Current context version is " << version;
+	result = std::make_shared<gpu::gl::GLBackend>();
+   
+    result->initInput(); 
+    //result->initTransform(); // TODO: uncomment
+
+    INSTANCE = result.get();
+    void* voidInstance = &(*result);
+    qApp->setProperty(GL_BACKEND_PROPERTY_NAME, QVariant::fromValue(voidInstance));
+
+    //gl::GLTexture::initTextureTransferHelper(); // TODO: uncomment
+    return result;
+}
+
+void GLBackend::initInput() {
+    if(!_input._defaultVAO) {
+        //glGenVertexArrays(1, &_input._defaultVAO);
+        qDebug() << "TODO: GLBackend.cpp:initInput glGenVertexArrays";
+    }
+    // glBindVertexArray(_input._defaultVAO);
+    qDebug() << "TODO: GLBackend.cpp:initInput glGenVertexArrays";
+    (void) CHECK_GL_ERROR();
+}
+
+
--- libraries/gpu-gl-android/src/gpu/gl/GLBackend.h
+++ libraries/gpu-gl-android/src/gpu/gl/GLBackend.h
@@ -16,26 +16,27 @@
 #include <list>
 #include <array>
 
-//#include <gl/Config.h>
+#include <gl/Config.h>
 
 #include <QtCore/QLoggingCategory>
+#include <gpu/Forward.h>
 
 #include <gpu/Context.h>
 
 namespace gpu { namespace gl {
 
-class GLBackend : public gpu::Backend {
+//class GLBackend : public gpu::Backend {
+class GLBackend : public Backend, public std::enable_shared_from_this<GLBackend> {
+
     using Parent = gpu::Backend;
     // Context Backend static interface required
     friend class gpu::Context;
     static void init() {}
     //static BackendPointer createBackend();
     //static gpu::Backend* createBackend() { return new GLBackend(); }
-    static BackendPointer createBackend() { return std::make_shared<GLBackend>(); }
+    static BackendPointer createBackend();
     // making it public static bool makeProgram(Shader& shader, const Shader::BindingSet& slotBindings) { return true; }
-protected:
-    explicit GLBackend(bool syncCache) : Parent() { }
-    
+
 public:
     GLBackend() { } /*: Parent() { } was protected*/
     ~GLBackend() { }
@@ -50,6 +51,9 @@ public:
     // Let's try to avoid to do that as much as possible!
     void syncCache() final { }
 
+    static const int MAX_NUM_ATTRIBUTES = Stream::NUM_INPUT_SLOTS;
+    static const int MAX_NUM_INPUT_BUFFERS = 16;
+
     // This is the ugly "download the pixels to sysmem for taking a snapshot"
     // Just avoid using it, it's ugly and will break performances
     virtual void downloadFramebuffer(const FramebufferPointer& srcFramebuffer, const Vec4i& region, QImage& destImage) final { }
@@ -59,6 +63,47 @@ public:
     virtual bool isTextureReady(const TexturePointer& texture) { return false; } // 170
 
     static bool makeProgram(Shader& shader, const Shader::BindingSet& slotBindings = Shader::BindingSet()) { return false; } // cpp 71
+
+
+protected:
+    explicit GLBackend(bool syncCache) : Parent() { }
+    virtual void initInput() final;
+
+    struct InputStageState {
+        bool _invalidFormat { true };
+        Stream::FormatPointer _format;
+
+        typedef std::bitset<MAX_NUM_ATTRIBUTES> ActivationCache;
+        ActivationCache _attributeActivation { 0 };
+
+        typedef std::bitset<MAX_NUM_INPUT_BUFFERS> BuffersState;
+        BuffersState _invalidBuffers { 0 };
+
+        Buffers _buffers;
+        Offsets _bufferOffsets;
+        Offsets _bufferStrides;
+        std::vector<GLuint> _bufferVBOs;
+
+        glm::vec4 _colorAttribute{ 0.0f };
+
+        BufferPointer _indexBuffer;
+        Offset _indexBufferOffset { 0 };
+        Type _indexBufferType { UINT32 };
+        
+        BufferPointer _indirectBuffer;
+        Offset _indirectBufferOffset{ 0 };
+        Offset _indirectBufferStride{ 0 };
+
+        GLuint _defaultVAO { 0 };
+
+        InputStageState() :
+            _buffers(_invalidBuffers.size()),
+            _bufferOffsets(_invalidBuffers.size(), 0),
+            _bufferStrides(_invalidBuffers.size(), 0),
+            _bufferVBOs(_invalidBuffers.size(), 0) {}
+    } _input;
+
+
 };
 
 } }
