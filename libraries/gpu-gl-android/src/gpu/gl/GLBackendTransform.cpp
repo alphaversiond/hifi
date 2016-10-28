04cc5fa8f5c37b9a7a1a2b73bdbe783be881d776
Cristian Luis Duarte <cristian.duarte.mb@gmail.com>
2016-10-28 15:59:15 -0300

    Less logs for better fps

--- libraries/gpu-gl-android/src/gpu/gl/GLBackendTransform.cpp
+++ libraries/gpu-gl-android/src/gpu/gl/GLBackendTransform.cpp
@@ -141,18 +141,18 @@ void GLBackend::updateTransform(const Batch& batch) {
 
     auto& drawCallInfoBuffer = batch.getDrawCallInfoBuffer();
     if (batch._currentNamedCall.empty()) {
-        qDebug() << "GLBackend::updateTransform empty";
+        //qDebug() << "GLBackend::updateTransform empty";
         (void)CHECK_GL_ERROR();
-        qDebug() << "GLBackend::updateTransform empty1";
+        //qDebug() << "GLBackend::updateTransform empty1";
         auto& drawCallInfo = drawCallInfoBuffer[_currentDraw];
         glDisableVertexAttribArray(gpu::Stream::DRAW_CALL_INFO); // Make sure attrib array is disabled
-        qDebug() << "GLBackend::updateTransform glDisableVertexAttribArray done";
+        //qDebug() << "GLBackend::updateTransform glDisableVertexAttribArray done";
         (void)CHECK_GL_ERROR();
         GLint current_vao, current_vbo, maxVertexAtribs;
         glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &current_vao);
         glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &current_vbo);
         glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &maxVertexAtribs);
-        qDebug() << "GLBackend::updateTransform glDisableVertexAttribArray currentvao " << current_vao << " current vbo " << current_vbo << " GL_MAX_VERTEX_ATTRIBS " <<  maxVertexAtribs;
+        //qDebug() << "GLBackend::updateTransform glDisableVertexAttribArray currentvao " << current_vao << " current vbo " << current_vbo << " GL_MAX_VERTEX_ATTRIBS " <<  maxVertexAtribs;
         glVertexAttribI4i(gpu::Stream::DRAW_CALL_INFO, drawCallInfo.index, drawCallInfo.unused, 0, 0);
 
         //int values[] = {drawCallInfo.index, drawCallInfo.unused};
@@ -165,11 +165,11 @@ void GLBackend::updateTransform(const Batch& batch) {
                 zero is bound to the GL_ARRAY_BUFFER buffer object binding point and
                 the pointer argument is not NULL.   TRUE
         */
-        qDebug() << "GLBackend::updateTransform glVertexAttribIPointer done";
+        //qDebug() << "GLBackend::updateTransform glVertexAttribIPointer done";
         (void)CHECK_GL_ERROR();
 
     } else {
-        qDebug() << "GLBackend::updateTransform else";
+        //qDebug() << "GLBackend::updateTransform else";
         glEnableVertexAttribArray(gpu::Stream::DRAW_CALL_INFO); // Make sure attrib array is enabled
         glBindBuffer(GL_ARRAY_BUFFER, _transform._drawCallInfoBuffer);
         glVertexAttribIPointer(gpu::Stream::DRAW_CALL_INFO, 2, GL_UNSIGNED_SHORT, 0,
