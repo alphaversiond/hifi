4560b721b13b77e248b9c8f4c2df02c72a2126bc
Cristian Luis Duarte <cristian.duarte.mb@gmail.com>
2016-11-29 23:24:41 -0300

    Set version 24 and a sky blue like color for cubes background

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -2274,7 +2274,7 @@ void Application::paintGL() {
 
             batch.setModelTransform(modelTransform);
             if (iCube == 0) {
-                batch.clearFramebuffer(gpu::Framebuffer::BUFFER_COLORS | gpu::Framebuffer::BUFFER_DEPTH, glm::vec4(0.0, 0.8, 0.0, 1.0), 100.0f, 0, true);
+                batch.clearFramebuffer(gpu::Framebuffer::BUFFER_COLORS | gpu::Framebuffer::BUFFER_DEPTH, glm::vec4(0.16, 0.47, 0.73, 1.0), 100.0f, 0, true);
             }
             batch.draw(gpu::TRIANGLES, 36); // 36
             renderArgs._context->appendFrameBatch(batch);
